f20ebe4b4f3b28b76e2ac9ace6a6758292951ca6
Holysmasher <shawnsummers1313@hotmail.com>
2024-07-02 14:21:41 -0600

    Finished Player Class

--- War.py
+++ War.py
@@ -42,9 +42,32 @@ class Deck:
 
 class Player:
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} Cards'
 
 
 new_deck = Deck()
 new_deck.shuffle()
+my_card = new_deck.deal_one()
+print(my_card)
+new_player = Player("Jose")
+new_player.add_cards(my_card)
+print(new_player)
+print(new_player.all_cards[0])
+new_player.add_cards([my_card, my_card, my_card, my_card, my_card])
+print(new_player)
+new_player.remove_one()
+print(new_player)
